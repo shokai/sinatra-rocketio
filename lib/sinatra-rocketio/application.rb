c180da205e9da24913218001fc256b6beda7652e
Sho Hashimoto <hashimoto@shokai.org>
2013-03-18 02:35:10 +0900

    delete routes of cometio.js and websocketio.js  #6

--- lib/sinatra-rocketio/application.rb
+++ lib/sinatra-rocketio/application.rb
@@ -19,6 +19,9 @@ module Sinatra
           content_type 'application/javascript'
           @js ||= ERB.new(Sinatra::RocketIO.javascript).result(binding)
         end
+        app.routes["GET"].delete_if{|route|
+          "/cometio/cometio.js" =~ route[0] or "/websocketio/websocketio.js" =~ route[0]
+        }
         Sinatra::RocketIO.emit :regist_events
       end
     end
