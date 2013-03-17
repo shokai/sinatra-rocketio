9564fa482029745282a95592403d836a31d8e48a
Sho Hashimoto <hashimoto@shokai.org>
2013-03-17 18:41:52 +0900

    put connection type-id on emit

--- lib/sinatra-rocketio/rocketio.rb
+++ lib/sinatra-rocketio/rocketio.rb
@@ -8,17 +8,17 @@ module Sinatra
 
     def self.sessions
       {
-        :websocketio => Sinatra::WebSocketIO.sessions,
-        :cometio => Sinatra::CometIO.sessions
+        :websocket => Sinatra::WebSocketIO.sessions,
+        :comet => Sinatra::CometIO.sessions
       }
     end
 
   end
 end
 EventEmitter.apply Sinatra::RocketIO
-Sinatra::CometIO.on :* do |event_name, args|
-  Sinatra::RocketIO.emit event_name, args
+Sinatra::CometIO.on :* do |event_name, *args|
+  Sinatra::RocketIO.emit event_name, [args, :comet].flatten
 end
-Sinatra::WebSocketIO.on :* do |event_name, args|
-  Sinatra::RocketIO.emit event_name, args
+Sinatra::WebSocketIO.on :* do |event_name, *args|
+  Sinatra::RocketIO.emit event_name, [args, :websocket].flatten
 end
--- sample/main.rb
+++ sample/main.rb
@@ -2,19 +2,19 @@ class ChatApp < Sinatra::Base
   register Sinatra::RocketIO
   io = Sinatra::RocketIO
 
-  io.on :connect do |session|
-    puts "new client <#{session}>"
-    push :chat, {:name => "system", :message => "new client <#{session}>"}
+  io.on :connect do |session, type|
+    puts "new client <#{session}> (type:#{type})"
+    push :chat, {:name => "system", :message => "new #{type} client <#{session}>"}
     push :chat, {:name => "system", :message => "welcome <#{session}>"}, {:to => session}
   end
 
-  io.on :disconnect do |session|
-    puts "disconnect client <#{session}>"
+  io.on :disconnect do |session, type|
+    puts "disconnect client <#{session}> (type:#{type})"
     push :chat, {:name => "system", :message => "bye <#{session}>"}
   end
 
-  io.on :chat do |data, from|
-    puts "#{data['name']} : #{data['message']}  (from:#{from})"
+  io.on :chat do |data, from, type|
+    puts "#{data['name']} : #{data['message']}  (from:#{from} type:#{type})"
     push :chat, data
   end
 
