dad7866e9ceb8cc92cd6a13b37cf08d1387b1b67
Sho Hashimoto <hashimoto@shokai.org>
2013-03-25 03:33:01 +0900

    refact ruby client #21

--- lib/sinatra/rocketio/client.rb
+++ lib/sinatra/rocketio/client.rb
@@ -15,29 +15,26 @@ module Sinatra
       attr_reader :settings, :type, :io
 
       def initialize(url, opt={:type => :websocket})
+        @type = opt[:type].to_sym
+        @io = nil
         @settings = JSON.parse HTTParty.get("#{url}/rocketio/settings").body
-        type = opt[:type].to_sym
-
-        if type == :websocket and @settings.include? 'websocket'
-          @type = :websocket
-          @io = Sinatra::WebSocketIO::Client.new @settings['websocket']
-        elsif type == :comet or @settings.include? 'comet'
-          @type = :comet
-          @io = Sinatra::CometIO::Client.new @settings['comet']
-        else
-          raise Error, "cannot find #{type} IO #{url}"
-        end
-        this = self
-        if @io
-          @io.on :* do |event_name, *args|
-            this.emit event_name, *args
-          end
-        end
+        @ws_close_timer = nil
         self
       end
 
       def connect
-        @io.connect
+        this = self
+        if @type == :websocket and @settings.include? 'websocket'
+          @io = Sinatra::WebSocketIO::Client.new(@settings['websocket']).connect
+        elsif type == :comet or @settings.include? 'comet'
+          @io = Sinatra::CometIO::Client.new(@settings['comet']).connect
+          @type = :comet
+        else
+          raise Error, "cannot find #{type} IO #{url}"
+        end
+        @io.on :* do |event_name, *args|
+          this.emit event_name, *args
+        end
         self
       end
 
